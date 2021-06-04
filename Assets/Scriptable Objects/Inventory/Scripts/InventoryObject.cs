d2a0251241f23790f59eb5e04b2ec17502d547f0
DESKTOP-TRU4BMJ\user <ponno145@gmail.com>
2021-06-04 11:02:50 +0900

    Inventory Version ratio Thanks for ratio

---
+++ Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+
+public class InventoryObject : ScriptableObject , ISerializationCallbackReceiver
+{
+    public ItemDatabaseObject database;
+    
+    public List<InventorySlot> Container = new List<InventorySlot>();
+    public void AddItem(ItemObject _item, int _amount)
+    {
+
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if (Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                return;
+            }
+        }
+
+
+        Container.Add(new InventorySlot(database.GetID[_item], _item, _amount));
+        
+    }
+
+    public void OnAfterDeserialize()
+    { 
+        for (int i = 0; i < Container.Count; i++)
+        {
+            Container[i].item = database.GetItem[Container[i].ID];
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
+}
+
+[System.Serializable]
+public class InventorySlot
+{
+    public int ID;
+    public ItemObject item;
+    public int amount;
+    public InventorySlot(int _id, ItemObject _item, int _amount)
+    {
+        ID = _id;
+        item = _item;
+        amount = _amount;
+    }
+    public void AddAmount(int value)
+    {
+        amount += value;
+    }
+}
